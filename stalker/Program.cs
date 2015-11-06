3181a886a6e7df53ec35ca2eadc0fbd9d4afb053
ebcolab <ebcolab@gmail.com>
2015-11-06 02:56:57 +0200

    pre-relise

---
+++ stalker/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace stalker
+{
+    class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			
+			string[] name = new string[3];      // array of name
+			Console.WriteLine("Input name");
+			for (int i = 0; i < name.Length; i++)
+			{
+				name[i] = Console.ReadLine();
+			}
+            
+			int[] cap = new int[3];             // array of capacity    
+			Console.WriteLine("Input Carrying capacity for each stalker");
+			for (int i = 0; i < cap.Length; i++)
+			{
+				cap[i] = Convert.ToInt32(Console.ReadLine());
+			}
+
+			int[,] art = {{4,5,4,3,6}, {2,6,4,5,3}, {3,1,5,3,4}};   //array of items
+			//Console.WriteLine("Input weight each artifacts for each stalker");
+			
+			
+			Stalker[] S = new Stalker[3];           // array of Stalker
+			for (int i = 0; i < S.Length; i++)
+			{
+				S[i] = new Stalker(name[i], cap[i]);
+				S[i].Show();
+				//Console.WriteLine("Speed with artefacts = " + s[i].carry(accrue[i));
+			}
+			for (int i = 0; i < 3; i++)
+			{
+				Console.WriteLine("\t Name " + S[i].name);
+				for (int j = 0; j < art.GetLength(1); j++)
+				{
+					S[i].accrue(art[i, j]);
+
+					Console.WriteLine("Weight = " + S[i].item + "\t Speed with artifacts =" + S[i].carry());
+				}
+			}
+			Console.ReadKey();
+		}
+	}
+}
---
+++ stalker/Stalker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stalker
+{
+	class Stalker
+	{
+		public double v0 = 1.42; //regular speep
+		public string name;     // name 
+		public double cap;      // capacity 
+		public double item;     // the number of artifacts
+         
+		public Stalker(string n, int m0)    // constructor 
+		{
+			name = n;
+			cap = m0;
+		}
+
+		public double carry()           // method: calculate speed with artifacts
+		{
+			double x, y, z;             // !!!REFACTORING
+			x = item * item;
+			y = cap * cap;
+			z = 1 + x / y;
+			return  v0 / (1 + item * item/cap * cap);
+		}
+
+		public void accrue(double a)    // method: calculate weight of artifacts for each stalker
+		{
+			item += a;
+		}
+		public void Show()              // method: output all data for each Stalker
+		{
+            Console.WriteLine("Name - " + name + " Carrying capacity = " + cap + " Weight of artifacts = " + item + " Regular speed = " + v0);
+		}
+
+	}
+		
+}
